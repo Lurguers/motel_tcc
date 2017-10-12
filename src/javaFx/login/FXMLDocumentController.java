5a2423b65cedda951bab85f294beebccfd5c6d17
Crodu <soratto.andre@gmail.com>
2017-10-12 16:18:55 -0300

    adicionado tela de login e imagens

---
+++ src/javaFx/login/FXMLDocumentController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaFx.login;
+
+import java.awt.event.KeyEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+
+/**
+ *
+ * @author andre
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Você clicou entrar");
+        
+        
+    }
+        
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
